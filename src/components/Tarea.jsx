c92d86a473c367320a0c7f27b3ea10f39c209152
unknown <jjrm.son8@gmail.com>
2022-02-28 17:24:08 -0500

    Decimo segundo commit - CSS para el calendario terminado y empezando CSS para botones de tareas

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Tarea({id, nombre, fecha, prioridad, eliminarTarea, setTareaEditar}) {
   return (
+
     <div className='tarea'>
-      <h4>{nombre}</h4>
+      <h3>{nombre}</h3>
       <p>{prioridad}</p>
-      <button onClick={() => eliminarTarea(id)}>Eliminar</button>
+      <button className='btn-tarea' onClick={() => eliminarTarea(id)}>Eliminar</button>
       <button onClick={() => setTareaEditar({id, nombre, prioridad, fecha})}>Editar</button>
     </div>
   )
